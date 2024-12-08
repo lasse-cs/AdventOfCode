dc745b534c49f16af81744207cf4305bb61321d9
Lasse <lasse.schmieding@gmail.com>
2024-12-08 08:23:14 +0000

    Solve Day 8

---
+++ AOC2024/day8/resonant_collinearity.py
@@ -0,0 +1,122 @@
+from argparse import ArgumentParser
+from typing import NamedTuple
+from collections import defaultdict
+from pathlib import Path
+from itertools import combinations
+from math import gcd
+
+
+class Location(NamedTuple):
+    row: int
+    column: int
+
+    def direction_to(self, other: "Location") -> "Direction":
+        return Direction(other.row - self.row, other.column - self.column)
+
+    def move_in(self, direction: "Direction") -> "Location":
+        return Location(self.row + direction.row, self.column + direction.column)
+
+
+class Direction(NamedTuple):
+    row: int
+    column: int
+
+    def __neg__(self) -> "Direction":
+        return Direction(-self.row, -self.column)
+
+    def reduce(self) -> "Direction":
+        common_div: int = gcd(self.row, self.column)
+        return Direction(self.row // common_div, self.column // common_div)
+
+
+class Antenna(NamedTuple):
+    location: Location
+    frequency: str
+
+
+class Map(NamedTuple):
+    rows: int
+    columns: int
+    antenna_locations: dict[str, list[Antenna]]
+
+    def find_point_antinodes(self) -> set[Location]:
+        antinodes: set[Location] = set()
+        for antenna_list in self.antenna_locations.values():
+            for combination in combinations(antenna_list, r=2):
+                first_antenna, second_antenna = combination
+                direction = first_antenna.location.direction_to(second_antenna.location)
+
+                first_antinode = second_antenna.location.move_in(direction)
+                if self._is_location_on_map(first_antinode):
+                    antinodes.add(first_antinode)
+
+                second_antinode = first_antenna.location.move_in(-direction)
+                if self._is_location_on_map(second_antinode):
+                    antinodes.add(second_antinode)
+
+        return antinodes
+
+    def find_line_antinodes(self) -> set[Location]:
+        antinodes: set[Location] = set()
+        for antenna_list in self.antenna_locations.values():
+            for combination in combinations(antenna_list, r=2):
+                first_antenna, second_antenna = combination
+                direction = first_antenna.location.direction_to(second_antenna.location)
+                forward_direction = direction.reduce()
+                backward_direction = -forward_direction
+                current_location: Location = first_antenna.location
+
+                # First go forwards
+                while self._is_location_on_map(current_location):
+                    antinodes.add(current_location)
+                    current_location = current_location.move_in(forward_direction)
+
+                # Then go backwards
+                current_location = first_antenna.location
+                while self._is_location_on_map(current_location):
+                    antinodes.add(current_location)
+                    current_location = current_location.move_in(backward_direction)
+
+        return antinodes
+
+    def _is_location_on_map(self, location: Location) -> bool:
+        if location.row < 0 or location.row >= self.rows:
+            return False
+        return location.column >= 0 and location.column < self.columns
+
+
+def parse(file: Path) -> Map:
+    with open(file, "r") as f:
+        return _parse(f.readlines())
+
+
+def _parse(lines: list[str]) -> Map:
+    rows: int = len(lines)
+    columns: int = len(lines[0].strip()) if rows > 0 else 0
+    antenna_locations: defaultdict[str, list[Antenna]] = defaultdict(list)
+
+    for row_index, row in enumerate(lines):
+        for column_index, cell in enumerate(row.strip()):
+            if cell == ".":
+                continue
+            location: Location = Location(row_index, column_index)
+            antenna: Antenna = Antenna(location, cell)
+            antenna_locations[cell].append(antenna)
+    return Map(rows, columns, dict(antenna_locations))
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("filename")
+
+    args = parser.parse_args()
+    file = Path(args.filename)
+
+    map: Map = parse(file)
+    antinodes: set[Location] = map.find_point_antinodes()
+    number_point_antinodes = len(antinodes)
+    print(f"There are {number_point_antinodes} point antinodes on the map")
+
+    line_antinodes: set[Location] = map.find_line_antinodes()
+    number_line_antinodes = len(line_antinodes)
+    print(f"There are {number_line_antinodes} line antinodes on the map")
---
+++ AOC2024/day8/test_resonant_collinearity.py
@@ -0,0 +1,61 @@
+from resonant_collinearity import parse, Location, Map, Antenna
+from pathlib import Path
+import pytest
+
+
+@pytest.fixture
+def simple_sample_input() -> Path:
+    return Path(__file__).resolve().parent / "files" / "test_simpler_input.txt"
+
+
+@pytest.fixture
+def sample_input() -> Path:
+    return Path(__file__).resolve().parent / "files" / "test_input.txt"
+
+
+@pytest.fixture
+def line_sample_input() -> Path:
+    return Path(__file__).resolve().parent / "files" / "test_line_input.txt"
+
+
+def test_parse(simple_sample_input: Path) -> None:
+    parsed_map: Map = parse(simple_sample_input)
+    expected_map: Map = Map(
+        10, 10, {"a": [Antenna(Location(3, 4), "a"), Antenna(Location(5, 5), "a")]}
+    )
+    assert parsed_map == expected_map
+
+
+def test_find_point_antinodes(simple_sample_input) -> None:
+    map: Map = parse(simple_sample_input)
+    antinodes: set[Location] = map.find_point_antinodes()
+    assert antinodes == {Location(1, 3), Location(7, 6)}
+
+
+def test_find_line_antinodes(line_sample_input) -> None:
+    map: Map = parse(line_sample_input)
+    antinodes: set[Location] = map.find_line_antinodes()
+    expected_antinodes = {
+        Location(0, 0),
+        Location(0, 5),
+        Location(1, 3),
+        Location(2, 1),
+        Location(2, 6),
+        Location(3, 9),
+        Location(4, 2),
+        Location(6, 3),
+        Location(8, 4),
+    }
+    assert antinodes == expected_antinodes
+
+
+def test_count_antinodes(sample_input) -> None:
+    map: Map = parse(sample_input)
+    antinodes: set[Location] = map.find_point_antinodes()
+    assert len(antinodes) == 14
+
+
+def test_count_line_antinodes(sample_input) -> None:
+    map: Map = parse(sample_input)
+    antinodes: set[Location] = map.find_line_antinodes()
+    assert len(antinodes) == 34
