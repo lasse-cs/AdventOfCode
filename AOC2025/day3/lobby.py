d590e34d1760e60fc6a463c9a160789eb87ba2e8
lasse-cs <lasse.schmieding@gmail.com>
2025-12-03 06:07:00 +0000

    Some cleanup

--- AOC2025/day3/lobby.py
+++ AOC2025/day3/lobby.py
@@ -8,40 +8,45 @@ class Bank:
     batteries: list[int]
 
     def joltage(self, num_allowed) -> int:
-        return self._joltage(num_allowed, 0, len(self.batteries))
+        joltage_array = self._joltage(num_allowed, 0, len(self.batteries))
+        return sum(num * 10**i for i, num in enumerate(reversed(joltage_array)))
 
     def _joltage(self, num_allowed, start, end):
-        if num_allowed <= 0:
-            return None
-        if end <= start:
-            return None
-        if num_allowed > end - start:
-            return None
+        if num_allowed <= 0 or num_allowed > end - start:
+            return []
         if num_allowed == end - start:
-            return int("".join(str(self.batteries[i]) for i in range(start, end)))
+            return self.batteries[start:end]
 
         maximum, idx = self._max_with_index(start, end)
         num_on_right = min(end - idx - 1, num_allowed - 1)
         num_on_left = num_allowed - num_on_right - 1
-        right_joltage = self._joltage(num_on_right, idx + 1, end)
-        left_joltage = self._joltage(num_on_left, start, idx)
-        result = str(left_joltage) if left_joltage else ""
-        result += str(maximum)
-        result += str(right_joltage) if right_joltage else ""
-        return int(result)
+        return (
+            self._joltage(num_on_left, start, idx)
+            + [maximum]
+            + self._joltage(num_on_right, idx + 1, end)
+        )
 
     def _max_with_index(self, start: int, end: int) -> tuple[int, int]:
-        curr_max = self.batteries[start]
-        curr_max_idx = start
+        curr_max, curr_max_idx = self.batteries[start], start
         for idx, battery in enumerate(self.batteries[start:end], start=start):
             if battery > curr_max:
-                curr_max = battery
-                curr_max_idx = idx
+                curr_max, curr_max_idx = battery, idx
         return curr_max, curr_max_idx
 
 
 def total_joltage(banks: list[Bank], num_allowed) -> int:
     return sum(bank.joltage(num_allowed) for bank in banks)
+
+
+def parse_banks(text: str) -> list[Bank]:
+    banks = []
+    for line in text:
+        line = line.strip()
+        if not line:
+            continue
+        batteries = [int(x) for x in line]
+        banks.append(Bank(batteries=batteries))
+    return banks
 
 
 def main():
@@ -50,13 +55,7 @@ def main():
     args = parser.parse_args()
 
     lines = args.filename.read_text().split("\n")
-    banks = []
-    for line in lines:
-        line = line.strip()
-        if not line:
-            continue
-        batteries = [int(x) for x in line]
-        banks.append(Bank(batteries=batteries))
+    banks = parse_banks(lines)
 
     two_joltage = total_joltage(banks, 2)
     print(two_joltage)
